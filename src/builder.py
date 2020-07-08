2c419dfc8b3efcd5de981359672e6907a22d95b0
aeg <aeg@amazon.com>
2020-07-08 11:07:04 +1000

    Reorder initilization

--- src/builder.py
+++ src/builder.py
@@ -46,26 +46,26 @@ class Builder:
         self.train_data = train_data
         self.val_data = val_data
         self.labels_file = labels_file
+        self.batch_size = batch_size
+        # Note: Since the max seq len for pos embedding is 512 , in the pretrained  bert this must be less than eq to 512
+        # Also note increasing the length greater also will create GPU out of mememory error
+        self._max_seq_len = max_seq_len
+        self.num_workers = num_workers or os.cpu_count() - 1
+        if self.num_workers <= 0:
+            self.num_workers = 0
+
         self._network = None
         self._train_dataloader = None
         self._train_dataset = None
         self._val_dataset = None
         self._val_dataloader = None
-
         self._trainer = None
         self._lossfunc = None
         self._optimiser = None
         self._label_mapper = None
-        self.num_workers = num_workers or os.cpu_count() - 1
-        self.batch_size = batch_size
-        if self.num_workers <= 0:
-            self.num_workers = 0
 
         self._bert_model_name = "bert-base-cased"
         self._token_lower_case = False
-        # Note: Since the max seq len for pos embedding is 512 , in the pretrained  bert this must be less than eq to 512
-        # Also note increasing the length greater also will create GPU out of mememory error
-        self._max_seq_len = max_seq_len
 
     def get_preprocessor(self):
         tokeniser = BertTokenizer.from_pretrained(self._bert_model_name, do_lower_case=self._token_lower_case)
