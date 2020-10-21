34449eea1ed3554c3f352cb252574f627b3e6837
aeg <aeg@amazon.com>
2020-10-21 11:44:17 +1100

    Update logging

--- src/builder.py
+++ src/builder.py
@@ -80,8 +80,7 @@ class Builder:
         state_dict = self.get_trainer().try_load_statedict_from_checkpoint()
 
         # Only load from BERT pretrained when no checkpoint is available
-        self._logger.info(
-            "No checkpoint models found.. Loading pretrained BERT {}".format(self._bert_model_name))
+        self._logger.info("checkpoint models found {}".format(state_dict is not None))
         self._network = BertModel(self._bert_model_name, self.get_label_mapper().num_classes,
                                   fine_tune=self.fine_tune, state_dict=state_dict)
 
